f2fc93fcc6ed782cc26d7992f861e1abc3ae273e
rajaofficelrk-cmd <rajaofficelrk@gmail.com>
2026-08-21 00:38:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello"
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -48,12 +55,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
     send_snapchat_message(
         "your_username",
         "your_password",
         "Hello!",
         "recipient_username"
-    )
+    )if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
